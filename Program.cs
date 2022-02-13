4f10ef31d3b4a41b3b9551ff31394fd1e4111a09
myursey <myursey@gmail.com>
2022-02-13 14:04:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SumSImpleNumber
+{
+    public class Program
+    {
+        static int SearchSimple(int[] Arr)
+        {
+            int emptyValues = 0;
+            bool marker;
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                marker = true;
+                for (int j = 2; j < Arr[i]; j++)
+
+                    if (Arr[i] % j == 0) marker = false;
+
+                if (marker) emptyValues += Arr[i];
+            }
+            return emptyValues;
+        }
+        static int SumSilpleNumber(int max)
+        {
+            int[] Arr = new int[max];
+            for (int i = 1; i < Arr.Length; i++)
+            {
+                Arr[i] = i + 1;
+            }
+            return SearchSimple(Arr);
+        }
+        static void Main(string[] args)
+        {
+            var max = 10;
+            var simpleSum = SumSilpleNumber(max);
+            Console.WriteLine(simpleSum);
+        }
+    }
+}
